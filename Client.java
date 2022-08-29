b2449dfefe8550b78ccc23040b60e85d8dfdeb57
Taylor <tmoss@una.edu>
2022-08-28 22:49:54 -0500

    updated

---
+++ Client.java
@@ -0,0 +1,39 @@
+import java.net.*;
+import java.io.*;
+
+ class Client {
+	public static void main(String[] args) {
+    	
+		try{
+			Socket socket = new Socket("localhost", 8080);	
+			BufferedReader in = new BufferedReader(
+			new InputStreamReader(socket.getInputStream())
+			);
+		
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader stdin = new BufferedReader(
+					new InputStreamReader(System.in));
+
+			String fromServer = null;
+			while((fromServer = in.readLine()) != null) {
+				System.out.println("Server: " + fromServer) ;
+					
+					String fromUser = stdin.readLine();
+					out.println(fromUser);
+					if(fromUser.equals("stop")) {
+				    break;
+					} 
+
+				}
+				System.out.println(fromServer);
+			} catch(IOException uhe) {
+				System.out.println("Bad IP");
+			}
+			System.out.println("client stopped");
+		}
+
+	}
+			
+		 
+
+
---
+++ Server.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server {
+
+    public static void main (String[] args) throws IOException {
+        System.out.println("start");
+        ServerSocket serverSocket = new ServerSocket(8080); // TODO: create a server socket
+        Socket clientSocket = serverSocket.accept(); // TODO: wait for a connection...
+        System.out.println("accept client connection..");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+
+        
+        PrintWriter out = new PrintWriter(
+            clientSocket.getOutputStream(), true); 
+        out.println("connection made");
+        // TODO: create an output stream (Writer) for the socket
+
+        String inputLine = null;
+        while ((inputLine = in.readLine()) !=null) {
+            System.out.println(inputLine.toUpperCase());
+            if (inputLine.equals ("stop")) {
+                break;
+            }
+            out.println(inputLine.toUpperCase());
+        }
+        System.out.println("sever stopped");
+}
+
+}
